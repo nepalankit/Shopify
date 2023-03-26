1769f9e918cd3eabda17e5bc72998956433cc030
nepalankit <nepalankit16@gmail.com>
2023-03-26 19:58:32 +0545

    added header footer body products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
-import react from "react";
-
+import React from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./Screens/HomeScreen";
 const App = () => {
   return (
     <>
-      <h1>Welcome to proshop</h1>
+      <Header />
+      <main className="py-3">
+        <Container></Container>
+        <HomeScreen />
+      </main>
+      <Footer />
     </>
   );
 };
