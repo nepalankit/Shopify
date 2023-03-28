9e636b8acc7e69006362229fe412b4a546b66e3c
nepalankit <nepalankit16@gmail.com>
2023-03-28 11:49:27 +0545

    added pages for each product, added some cart functionality

--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -1,11 +1,80 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import {
+  Row,
+  Col,
+  Card,
+  Image,
+  ListGroup,
+  Button,
+  ListGroupItem,
+} from "react-bootstrap";
+import Rating from "../components/Rating";
+import products from "../products";
+import { useParams } from "react-router-dom";
 
-function ProductScreen() {
+const ProductScreen = () => {
+  const { id } = useParams();
+  const product = products.find((p) => p._id === id);
+
   return (
-    <div>
-      <h1>Product</h1>
-    </div>
-  );
-}
+    <>
+      <Link className="btn btn-dark my-3" to="/">
+        Go Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h4>{product.name}</h4>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews}reviews`}
+              ></Rating>
+            </ListGroup.Item>
+          </ListGroup>
+          <ListGroup.Item>Price:${product.price}</ListGroup.Item>
+          <ListGroup.Item>Description:{product.description}</ListGroup.Item>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong> ${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroupItem>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
+                  </Col>
+                </Row>
+              </ListGroupItem>
 
+              <ListGroup.Item>
+                <Button
+                  className="btn-block"
+                  type="button"
+                  disabled={product.countInStock === 0}
+                >
+                  Add To Cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
 export default ProductScreen;
