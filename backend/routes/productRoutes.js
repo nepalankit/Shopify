209bd769a58ba952b213c4791d67ccb2022a40b9
nepalankit <nepalankit16@gmail.com>
2023-07-28 22:15:39 +0545

    user auth started

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,33 +1,11 @@
 import express from "express";
-import mongoose from "mongoose";
-import asyncHandler from "express-async-handler";
 const router = express.Router();
-import Product from "../model/productModel.js";
-//fetch all products
-//route GET /api/products
-//access public route
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
+import {
+  getProducts,
+  getProductById,
+} from "../controllers/productController.js";
 
-//fetch single products
-//route GET /api/products/:id
-//access public route
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("product not found");
-    }
-  })
-);
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductById);
 
 export default router;
