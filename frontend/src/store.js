15c80d491376f5d0fabfe4236b0fa81a67abd882
nepalankit <nepalankit16@gmail.com>
2023-07-29 21:52:29 +0545

    added user action constant reducers

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,18 +6,24 @@ import {
   productListReducer,
   producDetailsReducer,
 } from "./reducers/productReducer";
+import { userLoginReducer } from "./reducers/userReducers";
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: producDetailsReducer,
   cart: cartReducer,
+  user: userLoginReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartItems"))
   : [];
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
 
 const initialState = {
   cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfoFromStorage },
 };
 const middleware = [thunk];
 const store = createStore(
